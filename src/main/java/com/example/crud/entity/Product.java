1e444ae46a3ab64136976983d9017d7fc887be43
Gina <ginarm12@hotmail.com>
2022-05-11 21:58:14 -0500

    crud inicial

---
+++ src/main/java/com/example/crud/entity/Product.java
@@ -0,0 +1,27 @@
+package com.example.crud.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table (name = "product_tbl")
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
